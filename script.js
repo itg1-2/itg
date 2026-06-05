5a407b376798654a9be83b10162397e30bc47b26
itg1-2 <hannahgoldenberg2025@u.northwestern.edu>
2026-06-05 15:23:10 -0400

    Update script.js
    Article 4. Knicks vs Spurs NBA Finals Game 1

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ const articles = [
       content: `
       <p>After a long season, the NBA Finals have arrived, and despite all the doubt, the Knicks are making an appearance for the first time since 1999. Unfortunately for the Knicks, awaiting them on the other side is a 7’5 behemoth that has taken the league by storm.</p>
       <p>Polymarket currently gives the San Antonio Spurs a 63% chance to win the NBA Finals and DraftKings has them as -220 to win it all. However, I am not fully sold on what the oddsmakers are foretelling, as I am here to say that at the end of this series, the Knicks will be crowned NBA Champions.</p> 
-      <p>While I stand firm in my faith in the Orange and Blue, it is no secret why the Spurs are favorites. They have the best player in the series, and perhaps in the league, in Victor Wembanyama and overall their roster is more talented. Wembanyama is a force to be reckoned with on both ends, paired with a veteran guard in De’Aaron Fox to keep the offense running smoothly, a young duo of talented guards with Stephon Castle and Dylan Harper, and plenty of shooting and defense of Julian Champagnie and Devin Vassell.</p> 
+      <p>While I stand firm in my faith in the Orange and Blue, it is no secret why the Spurs are favorites. They have the best player in the series, and perhaps in the league, in Victor Wembanyama and overall their roster is more talented. Wembanyama is a force to be reckoned with on both ends, paired with a veteran guard in De’Aaron Fox to keep the offense running smoothly, a young duo of talented guards with Stephon Castle and Dylan Harper, and plenty of shooting and defense from Julian Champagnie and Devin Vassell.</p> 
       <p>The Spurs boast a well-constructed roster with incredible talent on both ends and they have proven themselves to be legitimate after a hard-fought seven game series against the Thunder. Truth be told, if both teams play their best ball, the Spurs will be crowned NBA champions.</p> 
       <p>But playing your best on the biggest stage is a big assumption and that's where the Knicks have the upper hand. The Knicks just played the best 11 game stretch in NBA history against some of the best teams in the East. The Orange and Blue have looked utterly dominant in just about every aspect - they have posted the highest net rating by a team in a playoff run in NBA history, leading the postseason in both offensive and defensive rating, have shot the highest three point and effective field goal percentage of any team so far this postseason, and are tied for the best assist to turnover ratio so far this postseason.</p> 
       <p>Simply put, the Knicks excel at maximizing their talent. They have played their best basketball every game since going down 2-1 to the Hawks with the exception of their poor first three quarters in Game 1 of the Eastern Conference Finals. They have managed to maximize the talent on their roster from stars like Brunson and KAT all the way down to guys on the bench like Landry Shamet, who was pivotal last series with his ability to torch the nets from downtown.</p> 
@@ -115,11 +115,28 @@ const articles = [
       
       `  
 
-}  
+},  
 
 
+{
 
-
+      title: "Flipping the Script: Brunson Comes Alive in the Clutch as Knicks’ Closing 11-0 Run Steals Game 1",
+      tag: "news",
+      excerpt: "Down 14 in San Antonio, Brunson shrugged off injuries to take over late in the fourth to stun the Spurs.",
+      date: "June 5, 2026",
+      author: "Andrew Vincenti",
+      content: `
+      <p>The oddsmakers called the Spurs heavy favorites. They said that the Knicks couldn’t handle Wemby. But a miraculous 11-0 closing run from the Orange and Blue just completely flipped the script on the NBA Finals.</p>
+      <p>Game 1 of the NBA Finals proves that if there's any player you want on your team for a playoff series, it's Jalen Brunson. In a gritty duel where the Knicks shot just 41% from the field and the Spurs shot a dismal 36%, Captain Clutch battled through injuries and put the team on his back, dropping 30 points with 13 coming in the 4th quarter alone.</p> 
+      <p>In a game that was up and down the whole way, Brunson struggled out of the gates, shooting just 5-of-15 from the field. Knicks fans were holding their breath in the first quarter after Brunson headed to the locker room with a knee injury in the first quarter before going down again in the second quarter with an injured ankle, but he was able to remain in the game.</p> 
+      <p>Like he has done all postseason, the Knicks’ captain came through again in the clutch. When the Spurs looked ready to take control in the fourth after a 9-0 run gave them the lead, Brunson sank a clutch three and drilled a dagger mid range shot to fuel an 11-0 Knicks run that iced the game.</p> 
+      <p>On the other end, Wembanyama struggled in his Finals debut. His 26 points seems impressive, but he shot just 6-of-21 from the field with six turnovers. The biggest issue is that Wemby couldn’t consistently get to the paint as the Knicks defense forced the 7’5 freak of nature to attempt nine three pointers on the night and he made just two of them. Wembanyama capped off his struggles with a costly turnover in the final minute that sealed the Game 1 loss for the Spurs.</p> 
+      <p>The Spurs supporting cast didn’t help pick up their star’s slack. Fox, Castle, and Vassell combined to shoot 2/15 from downtown. While Champagnie caught fire in the second quarter, after all of his trash talking, he completely disappeared in the second half. The only help Wemby got was from Dylan Harper, who looked like the best player on the floor for the first half of the game and ended the game with 16 points on 6-of-10 shooting.</p> 
+      <p>The Knicks, by contrast, had solid help all around. KAT dropped 18, 12, and 4 and, interestingly, found success attacking Wemby off the dribble. OG hit timely shots late, Landry Shamet played well offensively in extended minutes, and Josh Hart remained the glue guy with 15 rebounds, 6 assists, and 4 steals.</p>
+      <p>The Knicks defense was nothing short of incredible. As I said in my series preview, if the Knicks force Wemby off his spots and make him settle on the perimeter, the offense can go very dry, and that's exactly what happened in Game 1. But for all the good, the Orange and Blue’s offense looked bothered the whole game, struggling to put pressure on the rim with the unanimous Defensive Player of the Year locking down the paint.</p> 
+      <p>Looking ahead, the Knicks are going to have to figure out how to work around Wemby because Brunson can only throw the offense a life raft so many times. Hart’s inability to shoot certainly makes things awkward, but the Knicks have shown the ability to adjust and overcome the entire postseason, which is why I think the Knicks could very well be hoisting Larry O’Brien at the end of this season.</p> 
+      `  
+} 
   
 ];
 
